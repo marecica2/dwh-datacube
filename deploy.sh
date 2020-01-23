6083bc0584a079ff46edde8b0b2ba529f2e34978
marecica2 <marecica2@gmail.com>
2020-01-23 15:39:56 +0100

    DWH-12: Docker deployment

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@ cp settings.xml $HOME/.m2/settings.xml
 mvn -ntp deploy -DskipTests
 
 docker build -t marecica2/dwh-migrator -t marecica2/dwh-migrator:$SHA -f ./migrator/Dockerfile ./migrator
-#docker build -t marecica2/dwh-importer -t marecica2/dwh-migrator:$SHA -f ./importer/Dockerfile ./importer
-#docker build -t marecica2/dwh-react-app -t marecica2/dwh-migrator:$SHA -f ./react-app/Dockerfile ./react-app
+docker build -t marecica2/dwh-importer -t marecica2/dwh-migrator:$SHA -f ./importer/Dockerfile ./importer
+docker build -t marecica2/dwh-react-app -t marecica2/dwh-migrator:$SHA -f ./react-app/Dockerfile ./react-app
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
 
 docker push marecica2/dwh-migrator
-#docker push marecica2/dwh-importer
-#docker push marecica2/dwh-react-app
+docker push marecica2/dwh-importer
+docker push marecica2/dwh-react-app
