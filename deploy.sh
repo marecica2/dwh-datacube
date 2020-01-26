99ea607ef86dc30a5be9e6e2fb482c172c320730
marecica2 <marecica2@gmail.com>
2020-01-26 14:06:38 +0100

    DWH-12: k8s initial setup

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,23 @@
-cp settings.xml $HOME/.m2/settings.xml
-mvn -ntp deploy -DskipTests
+# #cp settings.xml $HOME/.m2/settings.xml
+mvn -ntp install -DskipTests
 
+export SHA=$(git rev-parse HEAD)
+
+docker build -t marecica2/dwh-react-app -t marecica2/dwh-react-app:$SHA -f ./react-app/Dockerfile ./react-app
 docker build -t marecica2/dwh-migrator -t marecica2/dwh-migrator:$SHA -f ./migrator/Dockerfile ./migrator
-docker build -t marecica2/dwh-importer -t marecica2/dwh-migrator:$SHA -f ./importer/Dockerfile ./importer
-docker build -t marecica2/dwh-react-app -t marecica2/dwh-migrator:$SHA -f ./react-app/Dockerfile ./react-app
+docker build -t marecica2/dwh-importer -t marecica2/dwh-importer:$SHA -f ./importer/Dockerfile ./importer
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
 
 docker push marecica2/dwh-migrator
+docker push marecica2/dwh-migrator:$SHA
 docker push marecica2/dwh-importer
+docker push marecica2/dwh-importer:$SHA
 docker push marecica2/dwh-react-app
+docker push marecica2/dwh-react-app:$SHA
+
+kubectl apply -f .k8s
+
+kubectl set image deployments/react-app-deployment client=marecica2/dwh-react-app:$SHA
+kubectl set image deployments/migrator-job migrator=marecica2/dwh-migrator:$SHA
+kubectl set image deployments/importer-deployment importer=marecica2/dwh-importer:$SHA
