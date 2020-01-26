728ead3ca978a74b9f9d1fd024cb1130555bf06c
marecica2 <marecica2@gmail.com>
2020-01-26 14:17:57 +0100

    DWH-12: k8s fix path

--- react-app/src/settings.js
+++ react-app/src/settings.js
@@ -1,5 +1,5 @@
 export const apiUrl = '/api';
-export const apiHost = 'http://localhost:8081';
+export const apiHost = '';
 export const pathPrefix = process.env.REACT_APP_PATH_PREFIX;
 export const portalApiPath = apiHost + apiUrl;
 
