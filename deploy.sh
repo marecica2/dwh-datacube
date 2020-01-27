5f2258b12c5f6b15d8fe1ec6fe591dc42b3ccba1
marecica2 <marecica2@gmail.com>
2020-01-27 13:10:03 +0100

    DWH-12: Fix

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,11 @@ docker build -t marecica2/dwh-importer -t marecica2/dwh-importer:$SHA -f ./impor
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
 
-docker push marecica2/dwh-migrator
+#docker push marecica2/dwh-migrator
+#docker push marecica2/dwh-importer
+#docker push marecica2/dwh-react-app
 docker push marecica2/dwh-migrator:$SHA
-docker push marecica2/dwh-importer
 docker push marecica2/dwh-importer:$SHA
-docker push marecica2/dwh-react-app
 docker push marecica2/dwh-react-app:$SHA
 
 kubectl apply -f .k8s
