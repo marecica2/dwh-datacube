4a15bc40002d4f37c8e72c2d0df88a94e7cc6c88
marecica2 <marecica2@gmail.com>
2020-01-27 13:55:07 +0100

    DWH-12: Deployment

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 # #cp settings.xml $HOME/.m2/settings.xml
-mvn -ntp install -DskipTests
+mvn -ntp clean install -DskipTests
 
 export SHA=$(git rev-parse HEAD)
 
@@ -12,12 +12,12 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
 #docker push marecica2/dwh-migrator
 #docker push marecica2/dwh-importer
 #docker push marecica2/dwh-react-app
+docker push marecica2/dwh-react-app:$SHA
 docker push marecica2/dwh-migrator:$SHA
 docker push marecica2/dwh-importer:$SHA
-docker push marecica2/dwh-react-app:$SHA
 
 kubectl apply -f .k8s
 
 kubectl set image deployments/react-app-deployment client=marecica2/dwh-react-app:$SHA
-kubectl set image deployments/migrator-deployment migrator=marecica2/dwh-migrator:$SHA
+kubectl set image pods/migrator-pod migrator=marecica2/dwh-migrator:$SHA
 kubectl set image deployments/importer-deployment importer=marecica2/dwh-importer:$SHA
