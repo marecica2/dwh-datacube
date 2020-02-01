586c50ebf72c30d9319693f6bd5001ba39ba67c5
marecica2 <marecica2@gmail.com>
2020-02-01 21:56:46 +0100

    DWH-15: Add Code coverage

--- common/src/main/java/org/bmsource/dwh/common/appstate/AppStateConfiguration.java
+++ common/src/main/java/org/bmsource/dwh/common/appstate/AppStateConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
--- importer/src/main/java/org/bmsource/dwh/ImporterApplication.java
+++ importer/src/main/java/org/bmsource/dwh/ImporterApplication.java
@@ -1,8 +1,14 @@
 package org.bmsource.dwh;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableAspectJAutoProxy
