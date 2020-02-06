cbae68606b6dad2504daf0c8fc61187555f87208
marecica2 <marecica2@gmail.com>
2020-02-06 11:28:29 +0100

    DWH-16: Codecov flags

--- react-app/jest/jest.config.js
+++ react-app/jest/jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
   cacheDirectory: '<rootDir>/node_modules/.cache/jest',
   verbose: true,
   notify: false,
+  collectCoverage: true,
   setupFiles: [`${configRootDir}/setup.js`],
   roots: ['<rootDir>/src'],
   snapshotSerializers: ['enzyme-to-json/serializer'],
@@ -11,10 +12,14 @@ module.exports = {
   transform: {
     '^.+\\.js?$': `${configRootDir}/jestWrapper.js`,
   },
-  moduleDirectories: ['node_modules', 'src'],
+  coverageReporters: ['json', 'text', 'lcovonly', 'clover'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `${configRootDir}/fileMock.js`,
     '\\.(css|less)$': `${configRootDir}/styleMock.js`,
   },
+  collectCoverageFrom : [
+    'src/**/*.{js,jsx}',
+    '!build/**',
+  ],
   transformIgnorePatterns: ['node_modules/(?!(@amcharts)/)'],
 };
