d527e2016ac2d681b1fab012ecd709e7c7020de3
marecica2 <marecica2@gmail.com>
2020-03-04 21:10:06 +0100

    DWH-17: Improve module naming convention

--- deploy.sh
+++ deploy.sh
@@ -4,20 +4,24 @@ mvn -ntp install -DskipTests
 export SHA=$(git rev-parse HEAD)
 
 docker build -t marecica2/dwh-react-app -t marecica2/dwh-react-app:$SHA -f ./react-app/Dockerfile ./react-app
-docker build -t marecica2/dwh-migrator -t marecica2/dwh-migrator:$SHA -f ./migrator/Dockerfile ./migrator
-docker build -t marecica2/dwh-importer -t marecica2/dwh-importer:$SHA -f ./importer/Dockerfile ./importer
+docker build -t marecica2/dwh-migrator -t marecica2/dwh-migrator:$SHA -f ./migrator/Dockerfile ./app-migrator
+docker build -t marecica2/dwh-olap -t marecica2/dwh-migrator:$SHA -f ./migrator/Dockerfile ./app-olap
+docker build -t marecica2/dwh-importer -t marecica2/dwh-importer:$SHA -f ./importer/Dockerfile ./app-importer
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
 
 #docker push marecica2/dwh-migrator
 #docker push marecica2/dwh-importer
+#docker push marecica2/dwh-olap
 #docker push marecica2/dwh-react-app
 docker push marecica2/dwh-react-app:$SHA
 docker push marecica2/dwh-migrator:$SHA
 docker push marecica2/dwh-importer:$SHA
+docker push marecica2/dwh-olap:$SHA
 
 kubectl apply -f .k8s
 
 kubectl set image deployments/react-app-deployment client=marecica2/dwh-react-app:$SHA
 kubectl set image pods/migrator-pod migrator=marecica2/dwh-migrator:$SHA
 kubectl set image deployments/importer-deployment importer=marecica2/dwh-importer:$SHA
+kubectl set image deployments/olap-deployment importer=marecica2/dwh-olap:$SHA
---
+++ lib-common/src/main/java/org/bmsource/dwh/common/multitenancy/impl/TenantContextInterceptor.java
@@ -0,0 +1,46 @@
+package org.bmsource.dwh.common.multitenancy.impl;
+
+import org.bmsource.dwh.common.multitenancy.Constants;
+import org.bmsource.dwh.common.multitenancy.TenantContext;
+import org.bmsource.dwh.common.multitenancy.TenantNotFoundException;
+import org.bmsource.dwh.common.portal.TenantRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class TenantContextInterceptor extends HandlerInterceptorAdapter {
+
+    Logger logger = LoggerFactory.getLogger(getClass());
+    {
+        logger.debug("Creating TenantInterceptor interceptor");
+    }
+
+    @Autowired
+    TenantRepository repository;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        String tenantUuid = request.getHeader(Constants.TENANT_HEADER);
+        String tenantSchema = tenantUuid != null ? repository.findById(tenantUuid)
+            .orElseThrow(() -> new TenantNotFoundException("Tenant not found"))
+            .getSchemaName() : null;
+        logger.debug("Set TenantContext: {}", tenantSchema);
+        TenantContext.setTenantSchema(tenantSchema);
+        return true;
+    }
+
+    @Override
+    public void postHandle(
+        HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
+        logger.debug("Clear TenantContext: {}", TenantContext.getTenantSchema());
+        TenantContext.setTenantSchema(null);
+    }
+
+}
