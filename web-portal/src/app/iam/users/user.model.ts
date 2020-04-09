aee70ba585c5eba4bd9ca2020c94cc08303a6997
marecica2 <marecica2@gmail.com>
2020-04-09 14:25:18 +0200

    DWH-18: Multiselect template

--- web-portal/src/app/iam/users/user.model.ts
+++ web-portal/src/app/iam/users/user.model.ts
@@ -1,6 +1,10 @@
+import { Tenant } from "../tenants/tenant.model";
+
 export interface User {
   username: string;
   firstName: string;
   lastName: string;
   email: string;
+  roles: string[];
+  tenants: Tenant[];
 }
--- web-portal/src/app/iam/users/users-table.component.ts
+++ web-portal/src/app/iam/users/users-table.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { UserService } from "./user.service";
 import { ColumnDefinition, ColumnType } from "../../shared/crudRepository/crudRepositoryApi";
+import { Tenant } from "../tenants/tenant.model";
+import { TenantService } from "../tenants/tenant.service";
 
 @Component({
   selector: 'iam-users-table',
@@ -19,6 +21,7 @@ export class UsersTableComponent {
     username: {
       type: ColumnType.STRING,
       label: 'User name',
+      formattedValue: (value: string) => value.toUpperCase(),
     },
     firstName: {
       type: ColumnType.STRING,
@@ -31,8 +34,20 @@ export class UsersTableComponent {
     email: {
       type: ColumnType.STRING,
     },
+    roles: {
+      type: ColumnType.MULTI_SELECT,
+      formattedValue: (roles: any[]) => {
+        return roles.map(obj => obj.name).join(", ");
+      },
+      accessor: 'name',
+    },
+    tenants: {
+      type: ColumnType.MULTI_SELECT,
+      formattedValue: (tenants: Tenant[]) => tenants.map((tenant: Tenant) => tenant.schemaName).join(", "),
+      accessor: 'schemaName',
+    },
   };
 
-  constructor(public service: UserService) {
+  constructor(private service: UserService, private tenantService: TenantService) {
   }
 }
