76df6ecff1402e3ecdcae4a7e208882f0ecfb5e3
marekb <marecica2@gmail.com>
2020-04-15 15:43:07 +0200

    DWH-19: HATEOAS headers for correct _links generation

--- app-security/src/main/java/org/bmsource/dwh/security/SecurityConfiguration.java
+++ app-security/src/main/java/org/bmsource/dwh/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package org.bmsource.dwh.security;
 import org.bmsource.dwh.security.model.User;
 import org.bmsource.dwh.security.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.Page;
@@ -13,6 +14,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 import javax.annotation.PostConstruct;
 
@@ -26,6 +28,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
+    }
+
+    @Bean
+    FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter()
+    {
+        FilterRegistrationBean<ForwardedHeaderFilter> bean = new FilterRegistrationBean<>();
+        bean.setFilter(new ForwardedHeaderFilter());
+        return bean;
     }
 
     @Bean
