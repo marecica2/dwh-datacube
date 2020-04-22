68e98bf2906abcdb594d3e11900c61149053f2f4
marekb <marecica2@gmail.com>
2020-04-22 11:26:33 +0200

    DWH-19: Fixed tests

--- app-migrator/src/main/java/org/bmsource/dwh/migrator/TenantRepository.java
+++ app-migrator/src/main/java/org/bmsource/dwh/migrator/TenantRepository.java
@@ -2,6 +2,8 @@ package org.bmsource.dwh.migrator;
 
 import org.bmsource.dwh.common.portal.Tenant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TenantRepository extends JpaRepository<Tenant, String> {
 }
--- app-security/src/main/java/org/bmsource/dwh/security/repository/UserRepository.java
+++ app-security/src/main/java/org/bmsource/dwh/security/repository/UserRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@RepositoryRestResource(exported = false)
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByUsername(String username);
